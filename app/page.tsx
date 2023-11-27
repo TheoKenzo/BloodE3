29cfbe7be017f45d92db0b7a608c9e2929f3b2f9
Theo Kenzo de Araújo <theokenzo35@gmail.com>
2023-11-27 19:27:45 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { MainPage } from './components/mainpage/mainpage'
 
 export default function Home() {
   return (
-    <body className='w-full h-full'>
+    <div className='w-[99vw] h-full'>
       <SideBar />
       <div className='flex flex-col justify-center items-center'>
         <MainPage />
       </div>
-    </body>
+    </div>
   )
 }
